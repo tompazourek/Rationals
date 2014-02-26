53aa62b4cb52716bdc14d51ab2129d25a5a9e59a
Tomáš Pažourek <tom.pazourek@gmail.com>
2014-02-26 21:02:14 +0100

    Implemented canonical form.

---
+++ RationalTests/CanonicalTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Rationals;
+
+namespace RationalTests
+{
+    [TestFixture]
+    public class CanonicalTests
+    {
+        [Test]
+        public void CanonicalForm()
+        {
+            // arrange
+            var rational = new Rational(2, 4);
+            
+            // action
+            var canonical = rational.CanonicalForm;
+
+            // assert
+            Assert.AreEqual(new Rational(1, 2), canonical);
+        }
+
+        [Test]
+        public void CanonicalForm_Irreducible()
+        {
+            // arrange
+            var rational = new Rational(4, 3);
+
+            // action
+            var canonical = rational.CanonicalForm;
+
+            // assert
+            Assert.AreEqual(rational, canonical);
+        }
+
+        [Test]
+        public void CanonicalForm_NegativeNumerator()
+        {
+            // arrange
+            var rational = new Rational(-2, 4);
+
+            // action
+            var canonical = rational.CanonicalForm;
+
+            // assert
+            Assert.AreEqual(new Rational(-1, 2), canonical);
+        }
+
+
+        [Test]
+        public void CanonicalForm_BothNegative()
+        {
+            // arrange
+            var rational = new Rational(-2, -4);
+
+            // action
+            var canonical = rational.CanonicalForm;
+
+            // assert
+            Assert.AreEqual(new Rational(1, 2), canonical);
+        }
+
+        [Test]
+        public void CanonicalForm_NegativeDenominator()
+        {
+            // arrange
+            var rational = new Rational(2, -4);
+
+            // action
+            var canonical = rational.CanonicalForm;
+
+            // assert
+            Assert.AreEqual(new Rational(-1, 2), canonical);
+        }
+
+        [Test]
+        public void CanonicalForm_ZeroNumerator()
+        {
+            // arrange
+            var rational = new Rational(0, -4);
+
+            // action
+            var canonical = rational.CanonicalForm;
+
+            // assert
+            Assert.AreEqual(new Rational(0, 1), canonical);
+        }
+    }
+}
---
+++ Rationals/Canonical.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    public partial struct Rational
+    {
+        /// <summary>
+        /// Cannonical form is irreducible fraction where denominator is always positive.
+        /// </summary>
+        /// <remarks>Canonical form of zero is 0/1.</remarks>
+        public Rational CanonicalForm
+        {
+            get
+            {
+                BigInteger gcd = BigInteger.GreatestCommonDivisor(_numerator, _denominator);
+
+                if (_denominator.Sign < 0)
+                    gcd = BigInteger.Negate(gcd); // ensures that canonical form is either positive or has minus in numerator
+                
+                var canonical = new Rational(_numerator / gcd, _denominator / gcd);
+                return canonical;
+            }
+        }
+    }
+}
