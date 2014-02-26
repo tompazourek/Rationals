6f1e260992c01c8df00fa67fe0c14167cefec375
Tomáš Pažourek <tom.pazourek@gmail.com>
2014-02-26 21:14:35 +0100

    Implemented implicit conversion.

---
+++ Rationals/ImplicitConversions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    public partial struct Rational
+    {
+        public static implicit operator Rational(int n)
+        {
+            return n != 0 ? new Rational(n) : Zero;
+        }
+
+        public static implicit operator Rational(uint n)
+        {
+            return n != 0 ? new Rational(n) : Zero;
+        }
+
+        public static implicit operator Rational(long n)
+        {
+            return n != 0 ? new Rational(n) : Zero;
+        }
+
+        public static implicit operator Rational(ulong n)
+        {
+            return n != 0 ? new Rational(n) : Zero;
+        }
+
+        public static implicit operator Rational(BigInteger n)
+        {
+            return !n.IsZero ? new Rational(n) : Zero;
+        }
+    }
+}
---
+++ Rationals/PredefinedRationals.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rationals
+{
+    public partial struct Rational
+    {
+        public static readonly Rational Zero = new Rational(0, 1);
+    }
+}
