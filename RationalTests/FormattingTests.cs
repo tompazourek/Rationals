ac9a342acaa55d068b559f8f47554ff54acba983
Tomáš Pažourek <tom.pazourek@gmail.com>
2014-02-28 21:09:09 +0100

    Implemented formatting as whole + fractional (W), formatting as canonical (C).

--- RationalTests/FormattingTests.cs
+++ RationalTests/FormattingTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -24,6 +25,32 @@ namespace RationalTests
 
             // assert
             Assert.AreEqual("3/4", formatted);
+        }
+
+        [Test]
+        public void Format_C()
+        {
+            // arrange
+            var rational = new Rational(new BigInteger(14), new BigInteger(7));
+
+            // action
+            string formatted = rational.ToString("C", CultureInfo.CurrentCulture);
+
+            // assert
+            Assert.AreEqual("2", formatted);
+        }
+
+        [Test]
+        public void Format_W()
+        {
+            // arrange
+            var rational = new Rational(new BigInteger(15), new BigInteger(7));
+
+            // action
+            string formatted = rational.ToString("W", CultureInfo.CurrentCulture);
+
+            // assert
+            Assert.AreEqual("2 + 1/7", formatted);
         }
     }
 }
--- Rationals/Formatting.cs
+++ Rationals/Formatting.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Rationals
 {
-    public partial struct Rational
+    public partial struct Rational : IFormattable
     {
         public override string ToString()
         {
@@ -19,6 +19,35 @@ namespace Rationals
                 return 0.ToString(CultureInfo.InvariantCulture);
 
             return string.Format(CultureInfo.InvariantCulture, "{0}/{1}", _numerator, _denominator);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (string.IsNullOrEmpty(format)) format = "F";
+            if (formatProvider == null) formatProvider = CultureInfo.CurrentCulture;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "F": // normal fraction
+                    return ToString();
+
+                case "W": // as whole + fractional part
+                    {
+                        var whole = WholePart;
+                        var fraction = FractionPart;
+                        if (whole.IsZero)
+                            return fraction.ToString();
+
+                        if (fraction.IsZero)
+                            return whole.ToString();
+
+                        return string.Format(formatProvider, "{0} + {1}", whole, fraction);
+                    }
+                case "C": // in canonical form
+                    return CanonicalForm.ToString();
+                default:
+                    throw new FormatException(String.Format("The {0} format string is not supported.", format));
+            }
         }
     }
 }
