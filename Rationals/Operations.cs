ea7d77d5fa402d089f1beafc8c7b49433020c6f7
Alexandre Rocha Lima e Marcondes <amarcondes@coextant.com>
2017-03-30 14:03:12 +0200

    Abs, Log and Log10 support

--- Rationals/Operations.cs
+++ Rationals/Operations.cs
@@ -175,5 +175,64 @@ namespace Rationals
                 return result;
             }
         }
+
+        /// <summary>
+        /// Gets the absolute value of the rational number
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static Rational Abs(Rational p)
+        {
+            if (p.IsZero)
+                return Zero;
+
+            BigInteger numerator = BigInteger.Abs(p.Numerator);
+            BigInteger denominator = BigInteger.Abs(p.Denominator);
+            var result = new Rational(ref numerator, ref denominator);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the base 10 logarithm of a rational number
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static double Log10(Rational p)
+        {
+            if (p.IsZero)
+                return double.NaN;
+
+            var result = BigInteger.Log10(p.Numerator) - BigInteger.Log10(p.Denominator);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the natural (base e) logarithm of a rational number
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static double Log(Rational p)
+        {
+            if (p.IsZero)
+                return double.NaN;
+
+            var result = BigInteger.Log(p.Numerator) - BigInteger.Log(p.Denominator);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the logarithm of a rational number
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="baseValue"></param>
+        /// <returns></returns>
+        public static double Log(Rational p, double baseValue)
+        {
+            if (p.IsZero)
+                return double.NaN;
+
+            var result = BigInteger.Log(p.Numerator, baseValue) - BigInteger.Log(p.Denominator, baseValue);
+            return result;
+        }
     }
 }
